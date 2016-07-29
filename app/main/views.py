ef4d95d82fcd0118ef140eedf2cb2fc2a9e88453
Junctionzc <junctionzc@gmail.com>
2016-07-29 15:04:45 +0800

    修复主页异常的问题

--- app/main/views.py
+++ app/main/views.py
@@ -15,12 +15,13 @@ def index():
     show_followed = False
     if current_user.is_authenticated:
         show_followed = bool(request.cookies.get('show_followed', ''))
+        posts_amount = (current_user.followed_posts.order_by(Post.timestamp.desc()).count(), \
+                    Post.query.order_by(Post.timestamp.desc()).count())
     if show_followed:
         query = current_user.followed_posts
     else:
         query = Post.query
-    posts_amount = (current_user.followed_posts.order_by(Post.timestamp.desc()).count(), \
-                    Post.query.order_by(Post.timestamp.desc()).count())
+        posts_amount = Post.query.order_by(Post.timestamp.desc()).count()
     pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page = current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out = False)
