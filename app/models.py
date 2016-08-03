fd85b22e0fbf50e39731ef23308a50dd04b6c293
Junctionzc <junctionzc@gmail.com>
2016-08-04 00:25:21 +0800

    编辑器插入图片

--- app/models.py
+++ app/models.py
@@ -304,10 +304,17 @@ class Post(db.Model):
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
+                        'h1', 'h2', 'h3', 'p', 'img']
+        attrs = {
+            '*': ['class'],
+            'a': ['href', 'rel'],
+            'img': ['src', 'alt']
+        }
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
-            tags=allowed_tags, strip = True))            
+            tags=allowed_tags,
+            attributes = attrs, 
+            strip = True))            
     
     @staticmethod
     def from_json(json_post):
